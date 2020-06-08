4abbd44a2c910135abbe1f3e885e069c9f2bf520
luizmarcelloxamarin <luiztelemicro@hotmail.com>
2020-06-08 20:56:17 -0300

    Aula 2 - Task

--- 01_Tasks/01_Tasks/Form1.Designer.cs
+++ 01_Tasks/01_Tasks/Form1.Designer.cs
@@ -28,13 +28,68 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnBaixar = new System.Windows.Forms.Button();
+            this.TxtResultado = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtBoxSite = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnBaixar
+            // 
+            this.btnBaixar.Location = new System.Drawing.Point(53, 306);
+            this.btnBaixar.Name = "btnBaixar";
+            this.btnBaixar.Size = new System.Drawing.Size(603, 102);
+            this.btnBaixar.TabIndex = 0;
+            this.btnBaixar.Text = "Fazer Download";
+            this.btnBaixar.UseVisualStyleBackColor = true;
+            this.btnBaixar.Click += new System.EventHandler(this.btnBaixar_Click);
+            // 
+            // TxtResultado
+            // 
+            this.TxtResultado.Location = new System.Drawing.Point(53, 104);
+            this.TxtResultado.Name = "TxtResultado";
+            this.TxtResultado.Size = new System.Drawing.Size(603, 171);
+            this.TxtResultado.TabIndex = 1;
+            this.TxtResultado.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(53, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(25, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Site";
+            // 
+            // txtBoxSite
+            // 
+            this.txtBoxSite.Location = new System.Drawing.Point(97, 28);
+            this.txtBoxSite.Name = "txtBoxSite";
+            this.txtBoxSite.Size = new System.Drawing.Size(559, 20);
+            this.txtBoxSite.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(701, 424);
+            this.Controls.Add(this.txtBoxSite);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.TxtResultado);
+            this.Controls.Add(this.btnBaixar);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnBaixar;
+        private System.Windows.Forms.RichTextBox TxtResultado;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtBoxSite;
     }
 }
 
--- 01_Tasks/01_Tasks/Form1.cs
+++ 01_Tasks/01_Tasks/Form1.cs
@@ -7,6 +7,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Net;
+using System.Net.Http;
+
 
 namespace _01_Tasks
 {
@@ -15,6 +18,25 @@ namespace _01_Tasks
         public Form1()
         {
             InitializeComponent();
+        }
+
+        //Rede, armazenamento, tela/display - Geram tempo de espera.
+        private async void btnBaixar_Click(object sender, EventArgs e)
+        {
+            //Quando usamos "TASKS", transformamos nosso fluxo(thread) em asyncrono,
+            //ou seja, não precisa esperar que ele seja executado para continuar o programa.
+            string endereco = txtBoxSite.Text;
+
+            //Classe para fazer downloads de conteúdos da internet
+            WebClient web = new WebClient();
+
+            //Download com método "syncrono"
+            //string html = web.DownloadString(endereco);
+
+            //Download com método "asyncrono"
+            string html = await web.DownloadStringTaskAsync(new Uri(endereco));
+
+            TxtResultado.Text = html;
         }
     }
 }
